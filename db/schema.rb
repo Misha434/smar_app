3b395eb66e9040684247a9098ea74018fcf44fee
misha434 <missymouse434@gmail.com>
2020-12-15 23:34:18 +0900

    Set model User validation
    Userモデルのバリデーション実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_15_131226) do
+ActiveRecord::Schema.define(version: 2020_12_15_142017) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "user_name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2020_12_15_131226) do
     t.date "birthday"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
   end
 
 end
