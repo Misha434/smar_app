b33a3f9cd98d03edd99b027b0501c669d553549e
misha434 <missymouse434@gmail.com>
2020-12-16 14:22:05 +0900

    Add User Sign-up Function, Partialize Setting head, header
    ユーザー登録機能の実装、application.html.slimのhead, header部分のパーシャル化をしたため

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/show'
+  # get 'users/new'
+  # get 'users/show'
   root 'static_pages#home'
   get '/about', to:'static_pages#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :users
+  # post 'users/new'
 end
