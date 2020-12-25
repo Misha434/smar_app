efe82d5335015d999fd1dc26be64c9c17a071e3f
misha434 <missymouse434@gmail.com>
2020-12-25 19:32:10 +0900

    Create User Login page
    User Loginを作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -19,6 +19,10 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
   private
     def user_params
       params.require(:user).permit(:user_name, :name, :email, :birthday, :password, :password_confirmation)
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-  # get 'users/new'
-  # get 'users/show'
   root 'static_pages#home'
   get '/about', to:'static_pages#about'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/signup', to:'users#new'
+  get '/login', to:'sessions#new'
+  post '/login', to:'sessions#create'
+  get '/logout', to:'sessions#destroy'
+
   resources :users
-  # post 'users/new'
 end
