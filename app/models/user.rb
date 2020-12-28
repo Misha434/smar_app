074afcb0e934c60538741e33f7f7d2b0222fd06d
misha434 <missymouse434@gmail.com>
2020-12-28 20:41:41 +0900

    Add user edit, update, index, and destroy actions
    ユーザー情報の編集・更新・一覧・削除機能の実装
    (pagenate & exec faker mot yet)

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
     uniqueness: true
   has_secure_password
   validates :birthday, presence: true
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, presence: true, length: { minimum: 6 }, allow_nil: true
 
   def User.digest(string)
     cost = ActiceModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST : BCrypt::Engine.cost
