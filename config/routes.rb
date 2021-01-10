9cb8fb21326d03deddb9811ebdd6bf50cbb135c8
misha434 <missymouse434@gmail.com>
2021-01-10 17:50:09 +0900

    Create mailer for activation and password_reset
    アクティベーションとパスワード再設定のテンプレ画面作成
    (メール設定前)

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,5 @@ Rails.application.routes.draw do
   delete '/logout', to:'sessions#destroy'
 
   resources :users
+  resources :accout_activations, only: [:edit]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_03_095020) do
+ActiveRecord::Schema.define(version: 2021_01_10_050303) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "user_name"
@@ -22,6 +22,9 @@ ActiveRecord::Schema.define(version: 2021_01_03_095020) do
     t.string "password_digest"
     t.boolean "admin", default: false
     t.string "remember_digest"
+    t.string "activation_digest"
+    t.boolean "activated", default: false
+    t.datetime "activated_at"
   end
 
 end
